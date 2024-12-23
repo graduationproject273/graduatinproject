ee659cf132c80faa2a968c987522bc394a3af2de
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2024-12-23 16:26:38 +0200

    add routes in project

---
+++ lib/core/routeing/routes.dart
@@ -0,0 +1,18 @@
+class Routes {
+  static const String onBoarding = '/onboarding';
+  static const String register = '/register';
+  static const String login = '/login';
+  static const String home = '/home';
+  static const String products = '/products';
+  static const String productDetails = '/product-details';
+  static const String categories = '/categories';
+  static const String blog = '/blog';
+  static const String blogDetails = '/blog-details';
+  static const String cart = '/cart';
+  static const String checkout = '/checkout';
+  static const String orderSuccess = '/order-success';
+  static const String profile = '/profile';
+  static const String refall = '/refall';
+  static const String adminDashboard = '/adminDashboard';
+  static const String additionlNotes = '/additionlNotes';
+}

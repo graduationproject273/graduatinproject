bf5147d6744d5996cc056611e8330f705f9687fc
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2024-12-24 07:57:24 +0200

    changes in project structure

--- lib/featrues/onbording/presentation/views/onboarding_view.dart
+++ lib/featrues/onbording/presentation/views/onboarding_view.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:gradution/core/routeing/routes.dart';
 
 class OnboardingView extends StatelessWidget {
   const OnboardingView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Onboarding'),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, Routes.register);
+          },
+          child: Text('Go to register'),
+        ),
+      ),
+    );
   }
 }

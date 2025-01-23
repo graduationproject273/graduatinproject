6af1c0abd159178a36c165ded3f0deeb7b3a237d
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2025-01-23 11:37:05 +0200

    refactor onboarding page

--- lib/featrues/onbording/presentation/views/widgets/body_onboarding.dart
+++ lib/featrues/onbording/presentation/views/widgets/body_onboarding.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gradution/core/routeing/routes.dart';
 import 'package:gradution/core/styles/extention.dart';
-import 'package:gradution/core/styles/textstyles.dart';
 import 'package:gradution/featrues/onbording/presentation/views/widgets/color_onboarding.dart';
-import 'package:gradution/featrues/onbording/presentation/views/widgets/texts_in_onboarding.dart';
-
 
 class BodyOnboarding extends StatelessWidget {
   final String text1;
@@ -12,72 +8,34 @@ class BodyOnboarding extends StatelessWidget {
   final bool isVisible;
   final String image;
   const BodyOnboarding({
-    super.key, required this.text1, required this.text2, required this.isVisible, required this.image,
+    super.key,
+    required this.text1,
+    required this.text2,
+    required this.isVisible,
+    required this.image,
   });
 
   @override
+
   /// This widget is the body of the onboarding screen,
   /// it shows the background image and the texts and the skip button
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        // Background image
-        Image.asset(image,
-          width: double.infinity,
-          height: double.infinity,
-          fit: BoxFit.cover,
-        ),
-
-        // Color onboarding widget
-        Positioned(
-          bottom: 0,
-          child: const coloronboarding(),
-        ),
-
-        // Texts in onboarding widget
-        Positioned(
-          bottom: 0,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: SizedBox(
-              height: context.height*.25,
-              width: context.width * .9,
-              child: Column(
-                children: [
-                  TextsInOnboarding(
-                    text1: text1,
-                    text2: text2,
-                  ),
-                ],
-              ),
-            ),
+        Positioned.fill(
+          child: Image.asset(
+            image,
+            fit: BoxFit.cover,
           ),
         ),
-
-        // Skip button
-        Visibility(
-          visible: isVisible,
-          child: GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, Routes.login);
-            },
-            child: Padding(
-              padding: const EdgeInsets.only(right: 16, top: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    'Skip',
-                    style: Textstyles.text2Inonboarding.copyWith(fontWeight: FontWeight.bold)
-                  ),
-                ],
-              ),
-            ),
+      
+        coloronboarding(
+            image: image,
+            text1: text1,
+            text2: text2, isVisible: isVisible,
           ),
-        ),
-      ],
+        ],
     );
   }
 }
-
--- lib/featrues/onbording/presentation/views/widgets/color_onboarding.dart
+++ lib/featrues/onbording/presentation/views/widgets/color_onboarding.dart
@@ -1,26 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gradution/core/routeing/routes.dart';
+import 'package:gradution/core/styles/extention.dart';
+import 'package:gradution/featrues/onbording/presentation/views/widgets/texts_in_onboarding.dart';
+
+import '../../../../../core/styles/textstyles.dart';
 
 // ignore: camel_case_types
 class coloronboarding extends StatelessWidget {
+  final String image;
+  final String text1;
+  final String text2;
+  final bool isVisible;
   const coloronboarding({
-    super.key,
+    super.key, required this.image, required this.text1, required this.text2, required this.isVisible,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-       width: MediaQuery.of(context).size.width, 
-       height: MediaQuery.of(context).size.height ,
-       decoration: BoxDecoration(
-         gradient: LinearGradient(
-           colors: [
-             Colors.transparent, // لون أغمق
-             Colors.black, // شفاف
-           ],
-           begin: Alignment.topCenter, // يبدأ من الأعلى
-           end: Alignment.bottomCenter, // ينتهي في الأسفل
-         ),
-       ),
-     );
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        
+        gradient: LinearGradient(
+          colors: [
+            Colors.transparent, // لون أغمق
+            Colors.black, // شفاف
+          ],
+          begin: Alignment.topCenter, // يبدأ من الأعلى
+          end: Alignment.bottomCenter, // ينتهي في الأسفل
+        ),
+      ),
+      child:  Padding(
+        padding:  EdgeInsets.symmetric(horizontal: 8.0.w),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Visibility(
+                                  visible: isVisible,
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      Navigator.pushNamed(context, Routes.login);
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(right: 16, top: 16),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: [
+                                          Text('Skip',
+                          style: Textstyles.text2Inonboarding
+                              .copyWith(fontWeight: FontWeight.bold)),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                      ),
+        Spacer(),
+                    TextsInOnboarding(
+                      text1: text1,
+                      text2: text2,
+                    ),
+                    SizedBox(
+                height: context.height*.15,
+                    )
+              
+                    ]),
+      )
+    );
   }
 }

d6b4a19ad01289ab0dfea6c189f1e99f38234ece
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2025-01-27 16:20:41 +0200

    add list cat

---
+++ lib/featrues/home/presentation/views/wedgets/data/models/categories_model.dart
@@ -0,0 +1,11 @@
+class CategoriesModel {
+ // رقم العنصر
+  final String name; // اسم العنصر
+  final String imageUrl; // رابط الصورة
+
+  CategoriesModel({
+  
+    required this.name,
+    required this.imageUrl,
+  });
+}

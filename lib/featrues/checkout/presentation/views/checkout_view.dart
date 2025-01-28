58f7ca407bf9de6e5095e50084bb140ec1d6bdff
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-01-28 15:01:53 +0200

    add checkout page

--- lib/featrues/checkout/presentation/views/checkout_view.dart
+++ lib/featrues/checkout/presentation/views/checkout_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:gradution/core/widgets/custom_appbar.dart';
+import 'package:gradution/featrues/checkout/presentation/views/widgets/checkout_view_body.dart';
 
 class CheckoutView extends StatelessWidget {
   const CheckoutView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: CustomAppBar(title: 'Checkout'),
+      body: const CheckoutViewBody(),
+    );
   }
 }

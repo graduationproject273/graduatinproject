d60d63a159fb9d58c115e822bcf3cd084ac9a15c
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2025-01-30 15:21:58 +0200

    change

--- lib/featrues/products/presentation/view/widgets/products_details_view_body.dart
+++ lib/featrues/products/presentation/view/widgets/products_details_view_body.dart
@@ -12,28 +12,40 @@ class ProductsDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Two Seater Luxury Sofa ',
-            style: Textstyles.text1Inonboarding
-                .copyWith(color: Colors.black, fontSize: 20.sp),
-          ),
-          const SizedBox(height: 8),
-          const PricesInDetailsProduct(oldPrice: '500', currentPrice: '400'),
-          const SizedBox(height: 8),
-          const NumberReviews(),
-           const SizedBox(height: 8),
-           Text('Minimal Sofa is made of by natural wood. The design' 
-           'that is very simple and minimal. This is truly one of '
-           'the best furnitures in any family for now. With 3 '
-           'different colors, you can easily select the best match '
-           'for your home. ',
-            style: Textstyles.textitemcat.copyWith(fontWeight: FontWeight.w200)
-                ,)
-        ],
+      padding: const EdgeInsets.all(16.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+             SizedBox(height: 25.h),
+            Image.asset(
+              Assets.imagesCameres,
+              height: 270.h,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+              SizedBox(height: 25.h),
+            Text(
+              'Two Seater Luxury Sofa ',
+              style: Textstyles.text1Inonboarding
+                  .copyWith(color: Colors.black, fontSize: 20.sp),
+            ),
+            const SizedBox(height: 8),
+            const PricesInDetailsProduct(oldPrice: '500', currentPrice: '400'),
+            const SizedBox(height: 8),
+            const NumberReviews(),
+             const SizedBox(height: 8),
+             Text('Minimal Sofa is made of by natural wood. The design' 
+             'that is very simple and minimal. This is truly one of '
+             'the best furnitures in any family for now. With 3 '
+             'different colors, you can easily select the best match '
+             'for your home. ',
+              style: Textstyles.textitemcat.copyWith(fontWeight: FontWeight.w200)
+                  ,),
+             SizedBox(height: 25.h),
+            const ButtonsInDetailsPage(),
+          ],
+        ),
       ),
     );
   }

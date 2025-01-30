8d93dc7ee9d637a0f923543585212ecf868c83f9
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-01-30 13:50:07 +0200

    add widget products view body

---
+++ lib/featrues/products/presentation/view/widgets/products_details_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductsDetailsViewBody extends StatelessWidget {
+  const ProductsDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

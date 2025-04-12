a955addaf53475b6760169432de4f1244d37e64e
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-04-12 15:13:30 +0200

    add single

--- lib/featrues/services/presentation/widgets/body_formuser_widet.dart
+++ lib/featrues/services/presentation/widgets/body_formuser_widet.dart
@@ -11,87 +11,89 @@ class BodyFormuserWidet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20),
-      child: Column(
-        children: [
-          CustomTextformfield(hintText:'First Name*' ,hintStyle: Textstyles.namereview.copyWith(color: Colors.grey, fontSize: 15), keyboardType: TextInputType.text, color: Colors.white,border: UnderlineInputBorder(
-      borderSide: BorderSide(
-        width: 2,
-        color: maincolor,
-      ),
-    ),),
-          const SizedBox(height: 20),
-          CustomTextformfield(hintText: 'Last Name*',hintStyle: Textstyles.namereview.copyWith(color: Colors.grey, fontSize: 15), keyboardType: TextInputType.emailAddress, color: Colors.white,border: UnderlineInputBorder(
-      borderSide: BorderSide(
-        width: 2,
-        color: maincolor,
-      ),
-    ),),
-          const SizedBox(height: 20),
-IntlPhoneField(
-  decoration: InputDecoration(
-    labelText: 'Mobile',
-    hintStyle: Textstyles.namereview.copyWith(
-      color: Colors.grey,
-      fontSize: 15,
-    ),
-    border: UnderlineInputBorder(
-      borderSide: BorderSide(
-        width: 2,
-        color: maincolor,
-      ),
-    ),
-    focusedBorder: UnderlineInputBorder(
-      borderSide: BorderSide(
-        width: 2,
-        color: maincolor,
-      ),
-    ),
-  ),
-  dropdownDecoration: BoxDecoration(
-    color: Colors.white, // الخلفية البيضاء للقائمة المنسدلة
-  ),
-  initialCountryCode: 'EG',
-  onChanged: (phone) {
-    print(phone.completeNumber);
-  },
-),
-
-          const SizedBox(height: 20),
-          CustomTextformfield(hintText: 'Email*',hintStyle:   Textstyles.namereview.copyWith(color: Colors.grey, fontSize: 15), keyboardType: TextInputType.visiblePassword, obscureText: true, color: Colors.white,
-          border: UnderlineInputBorder(
-      borderSide: BorderSide(
-        width: 2,
-        color: maincolor,
-      ),
-    ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomTextformfield(hintText:'First Name*' ,hintStyle: Textstyles.namereview.copyWith(color: Colors.grey, fontSize: 15), keyboardType: TextInputType.text, color: Colors.white,border: UnderlineInputBorder(
+        borderSide: BorderSide(
+          width: 2,
+          color: maincolor,
+        ),
+            ),),
+            const SizedBox(height: 20),
+            CustomTextformfield(hintText: 'Last Name*',hintStyle: Textstyles.namereview.copyWith(color: Colors.grey, fontSize: 15), keyboardType: TextInputType.emailAddress, color: Colors.white,border: UnderlineInputBorder(
+        borderSide: BorderSide(
+          width: 2,
+          color: maincolor,
+        ),
+            ),),
+            const SizedBox(height: 20),
+        IntlPhoneField(
+          decoration: InputDecoration(
+            labelText: 'Mobile',
+            hintStyle: Textstyles.namereview.copyWith(
+        color: Colors.grey,
+        fontSize: 15,
+            ),
+            border: UnderlineInputBorder(
+        borderSide: BorderSide(
+          width: 2,
+          color: maincolor,
+        ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+        borderSide: BorderSide(
+          width: 2,
+          color: maincolor,
+        ),
+            ),
           ),
-          const SizedBox(height: 20),
-          CustomTextformfield(hintText: 'Address*',hintStyle:   Textstyles.namereview.copyWith(color: Colors.grey, fontSize: 15), keyboardType: TextInputType.visiblePassword, obscureText: true, color: Colors.white
-          ,border: UnderlineInputBorder(
-      borderSide: BorderSide(
-        width: 2,
-        color: maincolor,
-      ),
-      
-    ),
+          dropdownDecoration: BoxDecoration(
+            color: Colors.white, // الخلفية البيضاء للقائمة المنسدلة
           ),
-          const SizedBox(height: 40),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text('Share with us your requirements*',style: Textstyles.namereview.copyWith(color: Colors.grey, fontSize: 15),),
-            ],
-          ),
-          const SizedBox(height: 20),
-          
-          CustomTextformfield(hintText: 'i.e I Wanna make my home smart',hintStyle: Textstyles.namereview.copyWith(
-            fontWeight: FontWeight.w400,
-            fontSize: 15,
-          ), keyboardType: TextInputType.text,
-          maxLines: 5,
-          colorborder: maincolor,
-          )
-        ],
+          initialCountryCode: 'EG',
+          onChanged: (phone) {
+            print(phone.completeNumber);
+          },
+        ),
+        
+            const SizedBox(height: 20),
+            CustomTextformfield(hintText: 'Email*',hintStyle:   Textstyles.namereview.copyWith(color: Colors.grey, fontSize: 15), keyboardType: TextInputType.visiblePassword, obscureText: true, color: Colors.white,
+            border: UnderlineInputBorder(
+        borderSide: BorderSide(
+          width: 2,
+          color: maincolor,
+        ),
+            ),
+            ),
+            const SizedBox(height: 20),
+            CustomTextformfield(hintText: 'Address*',hintStyle:   Textstyles.namereview.copyWith(color: Colors.grey, fontSize: 15), keyboardType: TextInputType.visiblePassword, obscureText: true, color: Colors.white
+            ,border: UnderlineInputBorder(
+        borderSide: BorderSide(
+          width: 2,
+          color: maincolor,
+        ),
+        
+            ),
+            ),
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text('Share with us your requirements*',style: Textstyles.namereview.copyWith(color: Colors.grey, fontSize: 15),),
+              ],
+            ),
+            const SizedBox(height: 20),
+            
+            CustomTextformfield(hintText: 'i.e I Wanna make my home smart',hintStyle: Textstyles.namereview.copyWith(
+              fontWeight: FontWeight.w400,
+              fontSize: 15,
+            ), keyboardType: TextInputType.text,
+            maxLines: 5,
+            colorborder: maincolor,
+            )
+          ],
+        ),
       ),
     );
   }

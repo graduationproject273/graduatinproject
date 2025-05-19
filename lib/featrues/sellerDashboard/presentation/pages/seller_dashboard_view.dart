3fed4f6f4e09fca0bd58f48f758a55b6fc19703d
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-05-19 19:27:56 +0300

    a

--- lib/featrues/sellerDashboard/presentation/pages/seller_dashboard_view.dart
+++ lib/featrues/sellerDashboard/presentation/pages/seller_dashboard_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gradution/featrues/sellerDashboard/presentation/widgets/dasboard_widet.dart';
-import 'package:gradution/featrues/sellerDashboard/presentation/widgets/seller_dashboard_drawer.dart';
-import 'package:gradution/featrues/sellerDashboard/presentation/widgets/fl_chart_in_seller.dart';
-import 'package:gradution/featrues/sellerDashboard/presentation/widgets/sller_dashboard_view_body.dart';
+
 
 
 class SellerDashboardView extends StatelessWidget {

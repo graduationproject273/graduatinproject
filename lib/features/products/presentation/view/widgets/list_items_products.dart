fce02e2f89d62d8051673f799182b7ed0c1f36c2
Abdelhamid Salah <abdelhamidsalah222@gmail.com>
2025-06-25 21:12:46 +0200

    aa

--- lib/features/products/presentation/view/widgets/list_items_products.dart
+++ lib/features/products/presentation/view/widgets/list_items_products.dart
@@ -6,45 +6,56 @@ import 'package:gradution/features/products/presentation/cubits/products_cubit/c
 import 'package:gradution/features/products/presentation/view/widgets/product_item.dart';
 
 // ignore: camel_case_types
-class listItemsProducts extends StatelessWidget {
+class listItemsProducts extends StatefulWidget {
   const listItemsProducts({
     super.key,
   });
 
   @override
+  State<listItemsProducts> createState() => _listItemsProductsState();
+}
+
+class _listItemsProductsState extends State<listItemsProducts> {
+ late  String select;
+  String searchQuery =''  ;
+  @override
   Widget build(BuildContext context) {
     return BlocBuilder<ProductsCubit, ProductsState>(
       builder: (context, state) {
         if (state is ProductsLoaded) {
-  return Expanded(
-    child: Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 150 / 270,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        itemBuilder: (context, index) {
-          return InkWell(
-              onTap: () {
-                GoRouter.of(context).push(Routes.productDetails);
-              },
-              child: ProductItem(
-                  productEntity:state.products[index] )); },
-        itemCount: 10,
-        shrinkWrap: true,
-        // physics: const NeverScrollableScrollPhysics(),
-      ),
-
-    ),
-  );
-}else if(state is ProductsError){
-  return Center(child: Text(state.message));
-}else{
-  return const Center(child: CircularProgressIndicator());
-}
+          state.products.where((product) {
+            return product.name
+                .toLowerCase()
+                .startsWith(searchQuery.toLowerCase());
+          }).toList();
+          return Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 150 / 270,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20,
+                ),
+                itemBuilder: (context, index) {
+                  return InkWell(
+                      onTap: () {
+                        GoRouter.of(context).push(Routes.productDetails);
+                      },
+                      child: ProductItem(productEntity: state.products[index]));
+                },
+                itemCount: 10,
+                shrinkWrap: true,
+                // physics: const NeverScrollableScrollPhysics(),
+              ),
+            ),
+          );
+        } else if (state is ProductsError) {
+          return Center(child: Text(state.message));
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
       },
     );
   }

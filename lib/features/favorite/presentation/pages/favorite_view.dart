822595cf1d584013a73d0bebcc26502cb6a4b02e
Abdelhamid Salah <abdelhamidsalah222@gmail.com>
2025-06-28 00:38:46 +0200

    add ui

--- lib/features/favorite/presentation/pages/favorite_view.dart
+++ lib/features/favorite/presentation/pages/favorite_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gradution/features/favorite/presentation/widgets/favoritre_view_body.dart';
 
 class FavoriteView extends StatelessWidget {
   const FavoriteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  FavoritreViewBody();
   }
 }
